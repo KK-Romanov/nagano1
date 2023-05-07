f9fb9b564e4379008b45c8154af37d3009267c2d
KK-Romanov <122021943+KK-Romanov@users.noreply.github.com>
2023-05-07 14:36:17 +0000

    22

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,13 +5,25 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
-    order = Order.find(params[:id])
-   order_details = order.order_details
-   if order.order_status == "入金確認"
-   order_details.update_all(making_status: "製作待ち")
-   end
+  #   order = Order.find(params[:id])
+  # order_details = order.order_details
+  # if order.order_status == "入金確認"
+  # order_details.update_all(making_status: "製作待ち")
+  # end
   # redirect_to admins_order_path(order.id)
+  @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: @order)
+    if @order.update(order_status_params)
+      if @order.order_status.include?("入金確認")
+         @order_details.update_all(making_status: "製作待ち")
+      end
+    flash[:success] = "制作ステータスを変更しました。"
+    redirect_to admin_order_path(@order)
+    else
+      render "show"
+    end
   end
+  
   
    private
    
