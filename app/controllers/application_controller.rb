07da69ad71032107fe8cbcca0e000fbd7accbb88
KK-Romanov <122021943+KK-Romanov@users.noreply.github.com>
2023-05-13 02:22:31 +0000

    最終調整1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
-    
+    # before_action :authenticate_customer!, except: [:top, :about ]
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      top_path
+    end
+  end
      
-
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
+
 end
